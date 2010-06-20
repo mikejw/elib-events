df52ba048ac75432ecfd01d95d679cad81ca036f
Mike Whiting <mail@mikejw.co.uk>
2010-06-20 14:27:27 +0000

    adding base template controller.

---
+++ DSection/BaseTemplate.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace ELib\DSection;
+use ELib\Model;
+
+use Empathy\Controller\CustomController;
+
+class BaseTemplate extends CustomController
+{
+  protected $section;
+  protected $data_item;
+
+  public function __construct($boot)
+  {
+    parent::__construct($boot);
+    $this->section = Model::load('SectionItem', $this);
+    $this->data_item = Model::load('DataItem', $this);
+  }
+
+
+
+}
+?>
