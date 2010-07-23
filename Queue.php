4fb796634052db13e04ea12645d40beddbe01fe6
Mike Whiting <mail@mikejw.co.uk>
2010-07-23 11:44:38 +0000

    small improvements to queuing.

--- Queue.php
+++ Queue.php
@@ -2,33 +2,34 @@
 
 namespace ELib;
 use ELib\Queue\DriverManager;
-
 use ELib\Queue\Job;
-use ELib\Queue\Tube;
 
 class Queue
 {  
   const DEFAULT_DRIVER = 'pheanstalk'; 
 
   private $driver;
+  private $tube;
   
-  public function __construct($host, $driver_name = null)
+  public function __construct($host, $tube = null, $driver_name = null)
   {
+    $this->tube = $tube;
     $this->driver = DriverManager::load($host, $driver_name);
   }
 
-  public function put($job_data, $tube = null)
-  {    
-    $j = new Job(array($job_data, $tube));
-
-    // check id matches output
-    //echo $j->getID();
-
-    $this->driver->put($j, $tube);    
+  public function setTube($tube)
+  {
+    $this->tube = $tube;
+    return $this;
   }
 
-  
+  public function put($job_data)
+  {    
+    $j = new Job(array($job_data, $this->tube));
+    $this->driver->put($j);    
+  }
 
+ 
 
 }
 ?>
