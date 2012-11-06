548bc59082662fc97fbcb26ed8fad46e6ea5859b
Mike Whiting <mail@mikejw.co.uk>
2012-11-06 09:46:36 +0000

    manual compatibility changes.

--- src/Empathy/ELib/Config.php
+++ src/Empathy/ELib/Config.php
@@ -1,27 +1,27 @@
 <?php
 
-namespace ELib;
+namespace Empathy\ELib;
 class Config
 {
 
-  public static function load($config_dir)
-  {
-    $config_file = $config_dir.'/elib.yml';
-    if(!file_exists($config_file))
-      {
-	die('Config error: '.$config_file.' does not exist');
-      }
+    public static function load($config_dir)
+    {
+        $config_file = $config_dir.'/elib.yml';
+        if(!file_exists($config_file))
+        {
+            die('Config error: '.$config_file.' does not exist');
+        }
     
-    $config = YAML::load($config_file);      
-    foreach($config as $index => $item)
-      {
-	if(!is_array($item))
-	  {
-	    $index = 'ELIB_'.$index;
-	    define(strtoupper($index), $item);
-	  }
-      }   
-  }
+        $config = YAML::load($config_file);      
+        foreach($config as $index => $item)
+        {
+            if(!is_array($item))
+            {
+                $index = 'ELIB_'.$index;
+                define(strtoupper($index), $item);
+            }
+        }   
+    }
 
   
 
--- src/Empathy/ELib/REST.php
+++ src/Empathy/ELib/REST.php
@@ -1,26 +1,26 @@
 <?php
 
-namespace ELib;
+namespace Empathy\ELib;
 
 class REST extends Curl
 { 
 
   
   
-  public function configure()
-  {
+    public function configure()
+    {
 
-    if($this->auth)
-      {
-	$auth_string = $this->user.':'.$this->pass;
-	curl_setopt($this->ch, CURLOPT_USERPWD, $auth_string);       
-      }
+        if($this->auth)
+        {
+            $auth_string = $this->user.':'.$this->pass;
+            curl_setopt($this->ch, CURLOPT_USERPWD, $auth_string);       
+        }
 
-    curl_setopt($this->ch, CURLOPT_POST, 0);
+        curl_setopt($this->ch, CURLOPT_POST, 0);
 
-    curl_setopt($this->ch, CURLOPT_URL, $this->url);
-    curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
-  }
+        curl_setopt($this->ch, CURLOPT_URL, $this->url);
+        curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
+    }
 
 
 }
