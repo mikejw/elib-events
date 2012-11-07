7b00b0b9261898d4b48ca27a38c971b3c59b4037
Mike Whiting <mail@mikejw.co.uk>
2012-11-07 10:29:37 +0000

    adding fix for tpl location.

--- src/Empathy/ELib/Util.php
+++ src/Empathy/ELib/Util.php
@@ -7,6 +7,10 @@ class Util
 {  
     public static function getLocation()
     {
-        return dirname(__FILE__);
+        //return dirname(__FILE__);
+
+
+        // fix for composer - how to deal with 'system install' templates?
+        return realpath(dirname(realpath(__FILE__)).'/../../../');
     }
 }
