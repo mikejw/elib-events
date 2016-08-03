13ccf5b87fc3adc8889292ecf47d9f06777bd3af
Mike Whiting <mail@mikejw.co.uk>
2016-08-03 14:51:11 +0100

    adding moule gen.

---
+++ src/Empathy/ELib/Gen/AdminEvents.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Empathy\ELib\Gen;
+
+class AdminEvents extends \Empathy\MVC\Util\ControllerGen
+{
+    protected $name = 'events';
+    protected $module = 'admin';
+    protected $parent = '\Empathy\ELib\Events\Controller';
+}
+
